e39529eedc687b71240aeccace2f7368336006ab
sjh9391985 <sjh9391985@gmail.com>
2023-12-22 13:09:27 +0900

    test: "repository 테스트 환경 구성"

---
+++ src/test/java/org/example/boardservice/repository/JpaRepositoryTest.java
@@ -0,0 +1,38 @@
+package org.example.boardservice.repository;
+
+import org.example.boardservice.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("SELECT 테스트")
+    @Test
+    void select_test() {
+        // given
+
+        // when
+
+        // then
+        
+    }
+}
