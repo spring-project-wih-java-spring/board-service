3ba2df58f65b92cc026bff4d0c35620962286cf3
sjh9391985 <sjh9391985@gmail.com>
2023-12-25 16:09:29 +0900

    feat: "spring data rest 세팅"

--- src/main/java/org/example/boardservice/repository/ArticleCommentRepository.java
+++ src/main/java/org/example/boardservice/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package org.example.boardservice.repository;
 
 import org.example.boardservice.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/org/example/boardservice/repository/ArticleRepository.java
+++ src/main/java/org/example/boardservice/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package org.example.boardservice.repository;
 
 import org.example.boardservice.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
