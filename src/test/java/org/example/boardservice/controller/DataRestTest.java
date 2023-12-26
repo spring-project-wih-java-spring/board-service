24e6825eaa422abecd7f47ba988939bf4ace87c9
sjh9391985 <sjh9391985@gmail.com>
2023-12-26 20:44:43 +0900

    test: "통합테스트 제외"

--- src/test/java/org/example/boardservice/controller/DataRestTest.java
+++ src/test/java/org/example/boardservice/controller/DataRestTest.java
@@ -1,21 +1,19 @@
 package org.example.boardservice.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Disabled("spring data rest 통합테스트는 불필요하므로 제외시킴.")
 @DisplayName("Data REST API 테스트")
 @Transactional // 기본적인 테스트들은 rollback 적용
 @AutoConfigureMockMvc // mock mvc 주입
