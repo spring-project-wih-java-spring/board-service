c3b4adf588eaa6e4714959773d4354b1910881e7
sjh9391985 <sjh9391985@gmail.com>
2023-12-27 10:14:49 +0900

    test: " 게시글 리스트 (게시판) 페이지 조회"

---
+++ src/test/java/org/example/boardservice/controller/ArticleControllerTest.java
@@ -0,0 +1,43 @@
+package org.example.boardservice.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view] [GET] - 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void get_article_viewpage() throws Exception {
+
+        // given
+
+        // when
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles")); // 데이터 검증 가능
+
+
+
+        // then
+
+    }
+}
