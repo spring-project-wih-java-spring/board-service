7183b0bc374b30f1a7c7bf438411f92d8a078a63
sjh9391985 <sjh9391985@gmail.com>
2023-12-29 22:55:36 +0900

    test: "로그인 테스트"

---
+++ src/test/java/org/example/boardservice/controller/AuthControllerTest.java
@@ -0,0 +1,33 @@
+package org.example.boardservice.controller;
+
+import org.example.boardservice.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("인증 컨트롤러 - 로그인, 로그아웃")
+@Import(SecurityConfig.class)
+@WebMvcTest
+public class AuthControllerTest {
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
+    @Test
+    void login() throws Exception {
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
