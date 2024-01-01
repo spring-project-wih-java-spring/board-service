83d194066148f2a145b94d88363474183f8e08d3
sjh9391985 <sjh9391985@gmail.com>
2024-01-01 13:17:37 +0900

    feat: "게시판 서비스 코드 추가"

--- src/main/java/org/example/boardservice/service/ArticleCommentService.java
+++ src/main/java/org/example/boardservice/service/ArticleCommentService.java
@@ -1,41 +1,31 @@
 package org.example.boardservice.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.boardservice.domain.Article;
 import org.example.boardservice.domain.type.SearchType;
+import org.example.boardservice.dto.ArticleCommentDto;
 import org.example.boardservice.dto.ArticleDto;
 import org.example.boardservice.dto.ArticleUpdateDto;
+import org.example.boardservice.repository.ArticleCommentRepository;
 import org.example.boardservice.repository.ArticleRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
-public class ArticleService {
+public class ArticleCommentService {
 
     private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
 
-    @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword) {
-        return Page.empty();
+    public List<ArticleCommentDto> searchArticleComment(long l) {
+        return Arrays.asList();
     }
 
-    public ArticleDto searchArticle(long l) {
-        return null;
-    }
-
-    public void savaArticle(ArticleDto articleDto) {
-    }
-
-    public void updateArticle(long l, ArticleUpdateDto articleUpdateDto) {
-    }
-
-    public void deleteArticle(long l) {
+    public void savaArticleComment(ArticleCommentDto articleCommentDto) {
     }
 }
--- src/test/java/org/example/boardservice/controller/MainControllerTest.java
+++ src/test/java/org/example/boardservice/controller/MainControllerTest.java
@@ -1,11 +1,15 @@
 package org.example.boardservice.controller;
 
 import org.example.boardservice.config.SecurityConfig;
+import org.example.boardservice.dto.ArticleDto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
